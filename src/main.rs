d921858c6fd6ffee4cf4bbcbd128d3c81580f1e4
Stålem, Sølve Rene <k01224@norsk-tipping.no>
2021-02-01 13:59:18 +0100

    added config.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+mod config;
 mod models;
 
 use actix_web::{HttpServer, App, web, Responder};
 use std::io;
 use crate::models::Person;
+use crate::config::Config;
+use dotenv::dotenv;
 
 async fn status() -> impl Responder {
   "{\"status\": \"UP\"}"
@@ -17,13 +20,18 @@ async fn get_person(_name: web::Path<String>) -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+
+  dotenv().ok();
+  let config = Config::from_env().unwrap();
+  println!("starting server at: http://{}:{}", config.server.host, config.server.port);
+
     HttpServer::new(|| {
 
         App::new()
         .route("/", web::get().to(status))
         .route("/person/{name}", web::get().to(get_person))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
     .await
 }
